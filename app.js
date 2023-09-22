6f18ada0a48925d5b4c866056de3eb49d4f393c6
Caetano Cesar <caetanocesar35@gmail.com>
2023-09-22 14:30:37 -0300

    Dinamizando classes de erro e sucesso do modal

--- app.js
+++ app.js
@@ -7,7 +7,43 @@ class Despesa{
 		this.descricao = descricao
 		this.valor = valor
 	}
+
+	validarDados(){
+		for(let i in this){
+			if(this[i] == undefined || this[i]== '' || this[i]==null){
+				return false
+			}
+		}
+		return true
+	}
 }
+
+class Bd{
+
+	constructor(){
+		let id = localStorage.getItem('id')
+
+		if(id === null){
+			localStorage.setItem('id', 0)
+		}
+	}
+
+	getProximoId(){
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId)+1
+	}
+
+	gravar(d){
+		let id = this.getProximoId()
+
+		localStorage.setItem(id,JSON.stringify(d))
+
+		localStorage.setItem('id', id)
+	}
+
+}
+
+let bd = new Bd()
 
 
 function cadastrarDespesa(){
@@ -27,10 +63,28 @@ function cadastrarDespesa(){
 		valor.value
 		)
 
-	gravar(despesa)
 
-}
+	if(despesa.validarDados()){
+		bd.gravar(despesa)
 
-function gravar(d){
-	localStorage.setItem('despesa', JSON.stringify(d))
+		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso'
+		document.getElementById('modal_btn').innerHTML = 'voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+		
+
+		$('#modalRegistraDespesa').modal('show')
+	} else{
+
+		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById('modal_titulo_div').className ='modal-header text-danger'
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos'
+		document.getElementById('modal_btn').innerHTML = 'voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+		
+
+		$('#modalRegistraDespesa').modal('show')
+	}
+	
 }
